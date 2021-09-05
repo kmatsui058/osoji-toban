3baf19c53094414fc684ebdb3d66d6d8d20b6934
kmatsui058 <kmatsui058@gmail.com>
2021-09-06 02:29:37 +0900

    見出しをenvからとらないようにする

--- src/modules/inviteCalender/inviteCalender.ts
+++ src/modules/inviteCalender/inviteCalender.ts
@@ -1,9 +1,10 @@
+import globalConfig from "../../classes/globalConfig";
 import { Schedule } from "../../classes/Schedule";
 
 export default function inviteCalendar(schedules: Schedule[]) {
     const calendar = CalendarApp.getDefaultCalendar()
     schedules.forEach((schedule)=>{
-        calendar.createAllDayEvent(`${schedule.member.name}さん${process.env.TOUBAN_NAME}`, schedule.date.toDate(), {guests: schedule.member.email})
+        calendar.createAllDayEvent(`${schedule.member.name}さん${globalConfig.title}`, schedule.date.toDate(), {guests: schedule.member.email})
     })
 
 }
